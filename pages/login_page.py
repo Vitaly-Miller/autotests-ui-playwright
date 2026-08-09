682c9319cdae8be48890e74123ef8731c2ad7a79
Vitaly Miller <vital.miller@gmail.com>
2026-08-08 22:43:42 -0700

    ✔️

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,8 @@
 """
 Login page
 """
-
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
-
-
 
 #=======================================================================================================================
 class LoginPage(BasePage):              # Дочерний класс (наследует класс BasePage)
