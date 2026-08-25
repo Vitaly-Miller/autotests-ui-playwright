3bfc7d131958fa0b2fbd02ac4091408c81a9acf3
Vitaly Miller <vital.miller@gmail.com>
2026-08-24 23:28:06 -0700

    ✔️

--- components/courses/create_course/toolbar_component.py
+++ components/courses/create_course/toolbar_component.py
@@ -2,10 +2,8 @@
 Create course page > [Toolbar] (component)
 """
 import allure
-
 from components.base_component import BaseComponent
 from playwright.sync_api import Page, expect
-
 
 #=======================================================================================================================
 """
--- components/dashboard/widget_component.py
+++ components/dashboard/widget_component.py
@@ -17,8 +17,8 @@ class DashboardWidgetComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str, chart_type: str):
         """
         :param page: Page
-        :param identifier: Unique part of locator (students | activities | courses | scores)
-        :param chart_type: Unique part of locator (bar | line | pie | scatter)
+        :param identifier: Unique part of locator (students | activities | courses | scores )
+        :param chart_type: Unique part of locator (   bar   |     line   |   pie   | scatter)
         """
         super().__init__(page)
 
@@ -30,7 +30,7 @@ class DashboardWidgetComponent(BaseComponent):
         # ------------------------------------- >>> [Element] path (for debug) -----------------------------------------
         self.widget_component = f'❌ Dashboard page > {self.widget_name}-widget'
         self.title_element = f'{self.widget_component} > [Title]'
-        self.chart_element = f'{self.widget_component} > {self.chart_name}-chart > [Chart]'
+        self.chart_element = f'{self.widget_component} > [{self.chart_name}-chart]'
 
         # ------------------------------------------------ ㉧ LOCATORS -------------------------------------------------
         self.title = page.get_by_test_id(f'{identifier}-widget-title-text')
