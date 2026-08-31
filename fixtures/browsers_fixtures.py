691dc21d67aa681e010d31b5edb55f51a71e9ab6
Vitaly Miller <vital.miller@gmail.com>
2026-08-31 13:22:13 -0700

    ✔️

--- fixtures/browsers_fixtures.py
+++ fixtures/browsers_fixtures.py
@@ -22,13 +22,16 @@ def page(request: SubRequest, storage_state: StorageState, playwright: Playwrigh
     browser = playwright.chromium.launch(                 # Создаем объект браузера на движке chromium c параметрами:
         channel='chromium',                               # - UI оболочка: 'chromium', 'chrome', 'msedge'
         headless=True,                                    # - True/False — НЕ/Показывать браузер
-        slow_mo=None)                                     # - Action delay (ms)
+        slow_mo=None                                      # - Action delay (ms)
+    )
     context = browser.new_context(                        # Создание браузерного окружения с Storage state:
         storage_state=storage_state,               # ┐    # - Storage state из фикстуры
         # storage_state='storage_state.json',      # ┘    # - Storage state из JSON-файла (optional)
         locale='en-US',                                   # - Website language (locale)
-        viewport=ViewportSize(width=1100, height=1200))   # - Window size
-    context.tracing.start(                                # Включаем Tracing для Playwright Trace Viewer
+        viewport=ViewportSize(width=1100, height=1200),   # - Window size
+        record_video_dir=f'tracing/videos/{request.node.name}'  # - Record video directory
+    )
+    context.tracing.start(                                # Tracing для Playwright Trace Viewer
         screenshots=True,                                 # - Screenshots
         snapshots=True,                                   # - Snapshots
         sources=True                                      # - Sources
@@ -39,13 +42,20 @@ def page(request: SubRequest, storage_state: StorageState, playwright: Playwrigh
         yield page                                        # Передаем page (на базе движка chromium)
 
     finally:                                              # Гарантия закрытия, если упадет.
-        context.tracing.stop(path=f'./tracing/{request.node.name}.zip')  # Сохраняем трейсинг в zip-файл (c именем текущего теста)
-        context.close()         # Закрываем context!
-        browser.close()         # Закрываем browser!
-        allure.attach.file(                               # 💾Прикрепляем трейсинг к Allure-отчету
-            f'./tracing/{request.node.name}.zip',         # - File path
+        context.tracing.stop(path=f'tracing/{request.node.name}.zip')  # Сохраняем трейсинг в zip-файл (c именем текущего теста)
+        allure.attach.file(                               # 💾 Прикрепляем трейсинг к Allure-отчету
+            source=f'tracing/{request.node.name}.zip',           # - File path
             name=f'{request.node.name}_trace',            # - Name in Allure-report (Tear down)
             attachment_type=allure.attachment_type.ZIP    # - File type - ZIP
+        )
+
+        context.close()                  # Закрываем context! (Playwright дописывает видео на диск)
+        browser.close()                  # Закрываем browser!
+
+        allure.attach.file(                              # 💾 Прикрепляем video к Allure-отчету (файл уже финализирован)
+            source=page.video.path(),    # NOQA          # - File path (через Page)
+            name=f'{request.node.name}_video',           # - Name in Allure-report (Tear down)
+            attachment_type=allure.attachment_type.WEBM  # - File type - WEBM
         )
 
 
@@ -75,8 +85,8 @@ def storage_state(playwright: Playwright):      # Используем встр�
     # ────────────────────────────────────────┘
 
     storage_state = context.storage_state()                             # v.1 - Storage state в переменную
-    # context.storage_state(path='storage_state.json')                  # v.2 - Storage state в 💾JSON-файл  (optional)
-    # storage_state = context.storage_state(path='storage_state.json')  # v.3 - Storage state в переменную + 💾JSON-файл  (optional)
+    # context.storage_state(path='storage_state.json')                  # v.2 - Storage state в 💾 JSON-файл  (optional)
+    # storage_state = context.storage_state(path='storage_state.json')  # v.3 - Storage state в переменную + 💾 JSON-файл  (optional)
 
     try:
         yield storage_state                     # Передаем Storage state
@@ -100,10 +110,13 @@ def page_guest(request: SubRequest, playwright: Playwright):   # Чистый (�
     browser = playwright.chromium.launch(                 # Создаем объект браузера на движке chromium c параметрами:
         channel='chromium',                               # - UI оболочка: 'chromium', 'chrome', 'msedge'
         headless=True,                                    # - True/False — НЕ/Показывать браузер
-        slow_mo=None)                                     # - Action delay (ms)
+        slow_mo=None                                      # - Action delay (ms)
+    )
     context = browser.new_context(                        # Создание браузерного окружения (NO Storage state):
         locale='en-US',                                   # - Website language (locale)
-        viewport=ViewportSize(width=1100, height=1200))   # - Window size
+        viewport=ViewportSize(width=1100, height=1200),   # - Window size
+        record_video_dir=f'tracing/videos/{request.node.name}' # - Record video directory
+    )
     context.tracing.start(                                # Включаем Tracing для Playwright Trace Viewer
         screenshots=True,                                 # - Screenshots
         snapshots=True,                                   # - Snapshots
@@ -115,13 +128,20 @@ def page_guest(request: SubRequest, playwright: Playwright):   # Чистый (�
         yield page                                        # Передаем page (на базе context)
 
     finally:                                              # Гарантия закрытия, если упадет
-        context.tracing.stop(path=f'./tracing/{request.node.name}.zip')  # Сохраняем трейсинг в zip-файл (c именем текущего теста)
-        context.close()                                   # Закрываем context!
-        browser.close()                                   # Закрываем browser!
+        context.tracing.stop(path=f'tracing/{request.node.name}.zip')  # Сохраняем трейсинг в zip-файл (c именем текущего теста)
         allure.attach.file(                               # 💾Прикрепляем трейсинг к Allure-report
-            f'./tracing/{request.node.name}.zip',         # - File path
+            source=f'tracing/{request.node.name}.zip',           # - File path
             name=f'{request.node.name}_trace',            # - Name in Allure-report (Tear down)
             attachment_type=allure.attachment_type.ZIP    # - File type - ZIP
         )
 
+        context.close()                  # Закрываем context! (Playwright дописывает видео на диск)
+        browser.close()                  # Закрываем browser!
+
+        allure.attach.file(                              # 💾 Прикрепляем video к Allure-отчету (файл уже финализирован)
+            source=page.video.path(),    # NOQA          # - File path (через Page) NOQA
+            name=f'{request.node.name}_video',           # - Name in Allure-report (Tear down)
+            attachment_type=allure.attachment_type.WEBM  # - File type - WEBM
+
+        )
 #=======================================================================================================================
